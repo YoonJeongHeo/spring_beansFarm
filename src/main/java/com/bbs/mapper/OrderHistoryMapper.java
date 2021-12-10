4b146f5c7928acaa6185afd1fcd3932f8aa5b7fe
baekseungyeop <se01163@naver.com>
2021-12-10 11:28:40 +0900

    마이페이지 개인 전체 주문내역 조회 / 마이페이지 컨트롤러 글자깨짐 수정

--- src/main/java/com/bbs/mapper/OrderHistoryMapper.java
+++ src/main/java/com/bbs/mapper/OrderHistoryMapper.java
@@ -3,12 +3,17 @@ package com.bbs.mapper;
 import java.util.List;
 
 import com.bbs.model.ProductDTO;
+import com.bbs.page.MypageCriteria;
 
 public interface OrderHistoryMapper {
 
-	// 주문내역 조회.
+	// 메인마이페이지 주문내역 조회. 5개까지 보임
 	public List<ProductDTO> myOrderHistory(Long m_no);
 
+	public List<ProductDTO> myAllOrderHistory(MypageCriteria cri);
+
+	public int getTotal(Long m_no);
+	
 
 
 }
